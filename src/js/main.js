58ee69b6004c41bf4b9319d5e77581f9a76fddc0
Gabe Jonson <sky3107@gmail.com>
2017-06-02 14:26:48 +0300

    Done

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,48 @@
 $(document).ready(function() {
-
+  forest.init();
 });
+
+var forest = (function() {
+  var firstContainer = $('.first-container');
+  var firstHeight = firstContainer.innerHeight() - 120;
+
+  return {
+    init: function() {
+      this.scrollWindow();
+      this.smoothScroll();
+    },
+
+    scrollWindow: function() {
+      var self = this;
+
+      $(window).on('scroll', function() {
+        var st = $(this).scrollTop();
+        var parralaxScroll = st / 40;
+
+        self.parralax(parralaxScroll);
+
+        if(st > firstHeight) $('header').addClass('sticky');
+        else $('header').removeClass('sticky');
+      });
+    },
+
+    parralax: function(parralaxScroll) {
+      $('.parralax').css({
+        'background-position': '50% ' + -parralaxScroll + 'px'
+      });
+      console.log(parralaxScroll)
+    },
+
+    smoothScroll: function() {
+      $('.next').bind('click', function(e){
+        e.preventDefault();
+
+        var anchor = $(this);
+
+        $('html, body').stop().animate({
+          scrollTop: $(anchor.attr('href')).offset().top - 80
+        }, 500);
+      });
+    }
+  }
+})();
